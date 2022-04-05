ed93ae0ddb106f4625ae76fd12889d8de9e96554
Cathy Lam <cathy27lam@gmail.com>
2022-04-04 20:40:46 -0400

    Updating all game folders to main again

--- components/quizTally.py
+++ components/quizTally.py
@@ -4,13 +4,11 @@ from emoji import emojize
 from colorama import init, Fore, Back, Style
 
 def total(value):
-    # do some logic to see which character you selected
 
     if value == 30:
         gamevars.character = gamevars.characters[0]
 
         print(emojize(Fore.RED + "You are :alien:  " + gamevars.character))
-        # add some emoji icons, or show the character image using the Pillow package
         spider_man = Image.open("spider_man.png")
         spider_man.show()
 
@@ -18,7 +16,6 @@ def total(value):
         gamevars.character = gamevars.characters[1]
 
         print(emojize(Fore.CYAN + "You are :cyclone:  " + gamevars.character))
-        # add some emoji icons, or show the character image using the Pillow package
         captain_america = Image.open("captain_america.png")
         captain_america.show()
 
@@ -26,6 +23,5 @@ def total(value):
         gamevars.character = gamevars.characters[2]
 
         print(emojize(Fore.GREEN + "You are :man:  "+ gamevars.character))
-        # add some emoji icons, or show the character image using the Pillow package
         dr_strange = Image.open("doctor_strange.png")
         dr_strange.show()
